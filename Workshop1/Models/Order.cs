bf3830ab564dc08ff9d98316760d9b289355618a
scorp-subu <36320227+scorp-subu@users.noreply.github.com>
2018-05-26 16:19:58 +0800

    Update Order.cs
    Chagning the order class

--- Workshop1/Models/Order.cs
+++ Workshop1/Models/Order.cs
@@ -11,6 +11,8 @@ namespace Workshop1.Models
         public string Dishname { get; set; }
         public string OrderSize { get; set; } = String.Empty;
         public List<string> Spices { get; set; } = new List<string>();
+        
+        //1st Commit
             
 
     }
